5b588512cdf6a48892712c2b0591cf1652191ed0
shrey <79036238+shreyzeous21@users.noreply.github.com>
2022-09-25 00:02:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var slider=document.getElementById("slider");
+var emoji=document.getElementById("emoji");
+var emoticons=["mood_bad 😔", "sentiment_very_dissatisfied ☹️", 
+    "sentiment_satisfied 😀","sentiment_satisfied_alt 💦🍆👌😊",
+    "sentiment_very_satisfied 👅🍑 🍆✊✊💦💦💦💦💦"
+	];
+slider.oninput=function(){
+    emoji.innerHTML=emoticons[slider.value];
+}
